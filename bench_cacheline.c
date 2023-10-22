be1e9fbe19bffc65b79a7ed86b212e8748b62cd5
Aleksey Cheusov <vle@gmx.net>
2023-10-22 22:10:46 +0300

    Simplify bench_cacheline

--- bench_cacheline.c
+++ bench_cacheline.c
@@ -2,13 +2,13 @@
 #include <stdint.h>
 #include <stdlib.h>
 
-#define STEP 999331
-#define COUNT 1000
+// #define COUNT (32*1024*4)
+#define COUNT 0x20000
 
 int main(int argc, char **argv) {
-    uint8_t *ptr = aligned_alloc(64, (COUNT + 63) / 64 * 64);
+    uint8_t *ptr = aligned_alloc(64, COUNT);
     if (!ptr) {
-        perror("malloc(3) failed");
+        perror("aligned_alloc(3) failed");
         return 1;
     }
 
@@ -20,17 +20,35 @@ int main(int argc, char **argv) {
         return 2;
     }
 
-    ptr += atoi(argv[0]);
-
+    size_t step = atoi(argv[0]);
     size_t index = 0;
     size_t sum = 0;
-    for (unsigned k = 0; k < 2000; ++k) {
+    for (unsigned k = 0; k < 5000; ++k) {
         for (unsigned i = 0; i < COUNT; ++i) {
-            uint8_t *bytes64 = ptr + index;
-            sum += *bytes64;
-            *bytes64 += 56;
+            sum += ptr[index];
+            index = (index + step) & (COUNT - 1);
 
-            index = (index + STEP) % COUNT;
+            sum += ptr[index];
+            index = (index + step) & (COUNT - 1);
+
+            sum += ptr[index];
+            index = (index + step) & (COUNT - 1);
+
+            sum += ptr[index];
+            index = (index + step) & (COUNT - 1);
+
+            sum += ptr[index];
+            index = (index + step) & (COUNT - 1);
+
+            sum += ptr[index];
+            index = (index + step) & (COUNT - 1);
+
+            sum += ptr[index];
+            index = (index + step) & (COUNT - 1);
+
+            sum += ptr[index];
+            index = (index + step) & (COUNT - 1);
+
         }
     }
 
